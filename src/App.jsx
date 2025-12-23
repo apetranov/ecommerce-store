c06af2fc4283c592b24704a4dd59c05b844f270e
Alexander Petranov <shasho68.5@gmail.com>
2025-12-23 15:32:25 +0200

    Start adding like feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, use } from 'react'
 import './App.css'
 function App() {
   const [products, setProducts] = useState([]);
   const [product, setProduct] = useState("");
+  const [likedProducts, setLikedProducts] = useState([]);
+  const [cartProducts, setCartProducts] = useState([]);
+  const [showLiked, setShowLiked] = useState(false);
+  const [showAll, setShowAll] = useState(true);
 
+  function addLiked(productId) {
+    if (likedProducts.find(product => product.id === productId)) {
+      return;
+    }
+
+    const toAdd = products.find(product => product.id === productId);
+
+    const likedCopy = [...likedProducts];
+
+    likedCopy.push(toAdd);
+
+    setLikedProducts(likedCopy);
+  }
 
   async function getAllProducts() {
     try {
@@ -22,16 +39,48 @@ function App() {
 
   return (
     <div className='main-div lato-regular'>
-        <input type="text" placeholder='Search product...' onChange={(e) => setProduct(e.target.value)} />
+      <div className='navbar'>
+        <span onClick={() => {
+          setShowLiked(false);
+          setShowAll(true);
+        }}>All products</span>
+        <span onClick={() => {
+          setShowLiked(true);
+          setShowAll(false);
+        }}>Liked</span>
+        <span>Shopping cart</span>
+      </div>
+        {showAll && <div className='showAllDiv'>
+          <input type="text" placeholder='Search product...' onChange={(e) => setProduct(e.target.value)} />
           <div className='productContainer'>
           {products.map((product) =>
               <div className='productDiv' key={product.id}>
                   <img src={`${product.thumbnail}`} alt="" />
                   <h4>{product.title}</h4>
                   <p>${product.price}</p>
+                  <div>
+                    <button onClick={() => addLiked(product.id)} id="likeBtn"> {likedProducts.find(prod => prod.id === product.id) ? 'Unlike' : 'Like' } </button>
+                    <button id="addToCartBtn">Add to cart</button>
+                  </div>
                 </div>
             )}
         </div>
+        </div>}
+        {showLiked && 
+          <div className='productContainer'>
+            {likedProducts.map((product) =>
+              <div className='productDiv' key={product.id}>
+                  <img src={`${product.thumbnail}`} alt="" />
+                  <h4>{product.title}</h4>
+                  <p>${product.price}</p>
+                  <div>
+                    <button onClick={() => addLiked(product.id)} id="likeBtn">Like</button>
+                    <button id="addToCartBtn">Add to cart</button>
+                  </div>
+                </div>
+            )}
+          </div>
+        }
     </div>
   )
 }

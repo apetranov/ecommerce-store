95332ae2929716689ba92586530e5834fab6135d
Alexander Petranov <shasho68.5@gmail.com>
2025-12-26 19:43:24 +0200

    Add not found page

---
+++ src/components/NotFoundPage.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function NotFoundPage() {
+  return (
+    <div 
+        style={
+            {
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+                gap: "1rem",
+                height: "100vh"
+            }
+        }
+    >
+        <h1>This page does not exist 😞</h1>
+        <Link to={"/"}>
+            <button id='backHome'>Go back Home</button>
+        </Link>
+    </div>
+  )
+}
+
+export default NotFoundPage
